794da061d0c9d275cb6e0ac640def4e3b60077bd
Project134 <141550087+Project134@users.noreply.github.com>
2025-05-20 14:32:53 +0530

    Add files via upload

--- process.py
+++ process.py
@@ -192,11 +192,11 @@ urls = read_json('naukri_urls')
 # current directory
 cwd = os.getcwd()+"/"
 
-job1_name = input('Enter name for job_1')
-job1_password = input('Enter pwd for job_1')
+# job1_name = input('Enter name for job_1')
+# job1_password = input('Enter pwd for job_1')
 
-job2_name = input('Enter name for job_2')
-job2_password = input('Enter pwd for job_2')
+# job2_name = input('Enter name for job_2')
+# job2_password = input('Enter pwd for job_2')
 
 # job1_name = ''
 # job1_password = ''
@@ -475,14 +475,14 @@ df_job_3 = pd.read_csv(cwd+'job_2.csv')
 
 df_job_3.to_csv(cwd+'job_3.csv',index=False)
 
+# %% [markdown]
+# ### modifying the job_data to
+
 # %%
 # read the dataframe
 
 df_job_3 = pd.read_csv(cwd+'job_3.csv')
 
-
-# %% [markdown]
-# ### modifying the job_data to
 
 # %%
 # keyword score
@@ -558,10 +558,7 @@ df_job_3['location_score'] = df_job_3['job_location'].apply(location_score)
 
 df_job_3 = df_job_3.sort_values(by=['keyword_score','company_rating','location_score','easy_apply','company_name'], ascending=[False,False, True,False,True])
 df_job_3['display_text'] = df_job_3['keyword_score'].astype(str).str.cat(df_job_3['company_rating'].astype(str), sep=' | ',na_rep='NA').str.cat(df_job_3['salary'].astype(str), sep=' | ',na_rep = 'NA').str.cat(df_job_3['job_title'].astype(str), sep=' | ',na_rep = 'NA').str.cat(df_job_3['company_name'].astype(str), sep=' | ',na_rep = 'NA').str.cat(df_job_3['job_location'].astype(str),sep=' | ',na_rep = 'NA')
-df_job_3['category'] = "All"
 
-# %%
-df_job_3.to_csv(cwd+'job_3.csv',index=False)
 
 # %%
 df_job_3.rename(columns={'job_url':'url',
@@ -577,13 +574,14 @@ required_columns = ['id', 'url',
                     'job_location', 'remote',
                     'keywords',
                     'apply_on_company_site','easy_apply',
-                    'keyskills_match', 'keyword_score', 'location_score','category']
+                    'keyskills_match', 'keyword_score', 'location_score']
 
 # %%
 df_job_3 = df_job_3[required_columns]
 
 # %%
 def modify_column(df,condition,col_to_modify,previous_value,new_value):
+    df = df.copy()
     if col_to_modify not in df.columns:
         df[col_to_modify] = previous_value
 
@@ -593,149 +591,26 @@ def modify_column(df,condition,col_to_modify,previous_value,new_value):
 
 
 # %%
-modify_column(df_job_3,'company_rating > 3',"category","All","good company")
+## category assignment 
+
+max_keyword_score = df_job_3['keyword_score'].max()
+
+category_conditions = {
+    'P1': "(location_score == 3) and (keyskills_match == 'ni-icon-check_circle') and (easy_apply == 'Apply')",
+    'P2': "(location_score == 1) and (keyskills_match == 'ni-icon-check_circle') and (easy_apply == 'Apply')",
+    'P1': "(keyword_score > @max_keyword_score - 10) and (easy_apply == 'Apply')"
+    
+}
 
 # %%
+for cat,condition in category_conditions.items():
+    df_job_3 = modify_column(df_job_3,condition,'category','ZZZ',cat)
 
+# %%
+df_job_3.sort_values(by=['category'],inplace=True)
 
 # %%
 df_job_3.to_json('n.json',orient='records')
-
-# %% [markdown]
-# ### create html
-
-# %% [markdown]
-# #### html function
-
-# %%
-# #function to create html
-
-# def generate_html_with_tiles(url_dict, filename=f'naukri'):
-#     # Begin writing the HTML content
-#     html_content = '''<!DOCTYPE html>
-# <html lang="en">
-# <head>
-#     <meta charset="UTF-8">
-#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-#     <title>URL Tiles</title>
-#     <style>
-#         .tile {
-#             display: inline-block;
-#             margin: 10px;
-#             padding: 20px;
-#             border: 1px solid #ccc;
-#             border-radius: 8px;
-#             background-color: #f9f9f9;
-#             cursor: pointer;
-#             text-align: center;
-#             min-width: 100px;
-#         }
-#         .clicked {
-#             background-color: lightgreen;
-#         }
-#     </style>
-# </head>
-# <body>
-#     <div id="tiles-container">
-# '''
-
-#     # Add tiles for each URL
-#     for display_text, url in url_dict.items():
-#         html_content += f'''
-#         <div class="tile" onclick="handleClick(this, '{url}')">
-#             {display_text}
-#         </div>
-# '''
-
-#     # Add script to handle clicks and storage
-#     html_content += '''
-#     </div>
-#     <script>
-#         function handleClick(tile, url) {
-#             let clickedTiles = JSON.parse(localStorage.getItem('clickedTiles')) || [];
-#             if (clickedTiles.includes(url)) {
-#                 // If already clicked, remove from clickedTiles and remove clicked class
-#                 clickedTiles = clickedTiles.filter(item => item !== url);
-#                 tile.classList.remove('clicked');
-#             } else {
-#                 // If not clicked, add to clickedTiles and add clicked class
-#                 clickedTiles.push(url);
-#                 tile.classList.add('clicked');
-#             }
-#             localStorage.setItem('clickedTiles', JSON.stringify(clickedTiles));
-#             window.open(url, '_blank');  // Open the URL in a new tab
-#         }
-
-#         // Mark previously clicked tiles
-#         window.onload = function() {
-#             let clickedTiles = JSON.parse(localStorage.getItem('clickedTiles')) || [];
-#             let tiles = document.querySelectorAll('.tile');
-#             tiles.forEach(tile => {
-#                 let url = tile.getAttribute('onclick').split("handleClick(this, '")[1].slice(0, -2);
-#                 if (clickedTiles.includes(url)) {
-#                     tile.classList.add('clicked');
-#                 }
-#             });
-#         }
-#     </script>
-# </body>
-# </html>'''
-
-#     # Write to the specified HTML file
-#     with open(f'{cwd}{filename}.html', 'w') as file:
-#         file.write(html_content)
-
-# %% [markdown]
-# #### creating html files
-
-# %%
-# # generate html files
-
-# def generate_html_from_df(df,filename):
-#     job = {}
-#     print(len(df))
-#     if len(df) == 0:
-#         return
-#     for i,row in df.iterrows():
-#         job[row['display_text']]=row['job_url']
-
-#     generate_html_with_tiles(job,filename)
-
-# %%
-# query_1 = {
-#     '1_naukri_apply':'apply_on_company_site != "Apply on company site"',
-#     '2_naukri_save':'apply_on_company_site == "Apply on company site"'
-# }
-
-# query_2 = {
-#     '_1':'keyskills_match == "ni-icon-check_circle"',
-#     '_2':'company_rating >= 3.0 or company_rating.isnull()'
-# }
-
-# query_3 = {
-#     '_1':'keyword_score >= 5'
-# }
-
-# query_4 = {
-#     '_1_remote':'remote == "Remote"',
-#     '_2_delhi_mumbai':'location_score == 2 or location_score==3',
-#     '_3_other_states':'location_score > 3',
-#     # '_2_mumbai':'location_score == 2',
-#     # '_3_delhi':'location_score == 3',
-#     # '_4_bangalore':'location_score == 4',
-#     # '_6_hyderabad':'location_score == 6'
-# }
-
-
-# %%
-# for q1_name,q1_query in query_1.items():
-#     for q2_name,q2_query in query_2.items():
-#         for q3_name,q3_query in query_3.items():
-#             for q4_name,q4_query in query_4.items():
-#                 f_name = q1_name + q2_name + q3_name + q4_name
-#                 temp_df = df_job_3.query(q1_query).query(q2_query).query(q3_query).query(q4_query)
-#                 generate_html_from_df(temp_df,f_name)
-
 
 # %%
 
